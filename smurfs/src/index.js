4393ed96e709fbab051b6d04d44b4e983ccd7fee
Michelle Scott <scottmm374@gmail.com>
2019-10-28 18:36:00 -0400

    Somehow reactstrap import didn't save, so saved it

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./components/App";
 import { reducer } from "./reducers/reducers";
